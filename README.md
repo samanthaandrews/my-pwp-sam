056c05a486b0c1a895782472b79260cc7cd9cdc5
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-13 10:02:52 -0700

    created documentation directory and milestone-1.php document; added description to README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-pwp-sam
 Personal Website Project
+This is my one page personal website project.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sam
+ * Date: 1/13/18
+ * Time: 9:55 AM
+ */
