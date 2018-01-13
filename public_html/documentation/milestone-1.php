43685ce27598da8511f76d0e07227c9df0971c25
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-13 12:13:55 -0700

    wrote pwp purpose, audience, goal and persona in php file. checked for errors using validator.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,3 +5,35 @@
  * Date: 1/13/18
  * Time: 9:55 AM
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Sam's Personal Website Project</title>
+	</head>
+	<body>
+		<h1>Sam's Personal Website - Milestone 1</h1>
+		<h2>Purpose</h2>
+		<p>To show my skill and talent as a full stack web developer and designer. To showcase my individuality and
+			creativity.</p>
+		<h2>Audience</h2>
+		<p>Potential employers</p>
+		<h2>Goal</h2>
+		<p>To acquire an internship or employment (part-time or full-time) as a web developer in Albuquerque and
+			beyond!</p>
+		<h2>Persona</h2>
+		<p>Michelle Sharp</p>
+		<p>Woman</p>
+		<p>45 years old</p>
+		<p>Michelle is a full-stack developer and UI/UX designer. She has been coding professionally for over 20 years.
+			She is currently the head of the design and development team at a mid-size tech company in Albuquerque. She is
+			looking to hire a junior full stack developer to fill a new role in the growing company.</p>
+		<p>Michelle has hired many employees over her career. She looks for individuals who can demonstrate their
+			eagerness to learn and ability to adapt to change. She is looking for someone who wants to grow with the
+			company for at least 2-5 years. She likes clean, easy-to-read code and functional, streamlined design.</p>
+		<p>After looking at my site, Michelle will use the contact form to request a time to interview me for the junior
+			full stack developer role.</p>
+	</body>
+</html>
+
