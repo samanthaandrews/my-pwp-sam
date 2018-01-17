1f3099a510d19cd5e97d5060d7dfd0df1c92b640
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-17 13:10:05 -0700

    added more detailed information to persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -26,14 +26,23 @@
 		<p>Michelle Sharp</p>
 		<p>Woman</p>
 		<p>45 years old</p>
-		<p>Michelle is a full-stack developer and UI/UX designer. She has been coding professionally for over 20 years.
-			She is currently the head of the design and development team at a mid-size tech company in Albuquerque. She is
-			looking to hire a junior full-stack developer to fill a new role in the growing company.</p>
-		<p>Michelle has hired many employees over her career. She looks for individuals who can demonstrate their
-			eagerness to learn and ability to adapt to change. She is looking for someone who wants to grow with the
-			company for at least 2-5 years. She likes clean, easy-to-read code and functional, streamlined design.</p>
-		<p>After looking at my site, Michelle will use the contact form to request a time to interview me for the junior
-			full-stack developer role.</p>
+		<p>Profession: Michelle is the team lead of a design and development team at a mid-size web development company in
+			Albuquerque.
+		</p>
+		<p><strong>Technology:</strong> Michelle uses a 2017 MacBook pro that powers dual monitors.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Organized, but has a lot going on. Relies heavily on portfolio and
+			github repositories in making hiring decisions. Doesn't attend as many tech meetup events as she would like, so
+			she is more likely to hire someone through a referral from a friend or colleague.</p>
+		<p><strong>Frustrations and Needs:</strong> Michelle has hired many employees over her career. She looks for
+			individuals who can demonstrate their eagerness to learn and ability to adapt to change. She is looking for
+			someone who wants to grow with the company for at least 2-5 years. She likes clean, easy-to-read code and
+			functional, streamlined design.</p>
+		<p><strong>Goals:</strong> Michelle is looking to hire a junior full-stack developer with two to three years
+			experience. The company is growing, taking on more clients than ever, so she is hoping to hire someone quickly.
+			This week she is looking at resumes and portfolios. After looking at my site, Michelle will send an email
+			requesting a time to interview me for the junior full-stack developer role.</p>
+		<p><strong>User Story:</strong> As a user, I need to determine quickly whether this person is a candidate for the
+			full stack developer position.</p>
 	</body>
 </html>
 
