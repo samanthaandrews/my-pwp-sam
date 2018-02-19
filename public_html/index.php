b4ed6415ef7100c17823eb5b12911a7e2b095d45
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-18 22:39:50 -0700

    added content form

--- public_html/index.php
+++ public_html/index.php
@@ -31,73 +31,86 @@
 				<h1>SAMANTHA <br> ANDREWS</h1>
 				<div class="row">
 					<div class="col-md-3 text-center">
-						<h2><span class="underline-development">DEVELOPMENT</span></h2>
+						<h2><a href="#development"><span class="underline-development">DEVELOPMENT</span></a></h2>
 					</div>
 					<div class="col-md-3 text-center ">
-						<h2><span class="underline-design">DESIGN</span></h2>
+						<h2><a href="#design"><span class="underline-design">DESIGN</span></a></h2>
 					</div>
 					<div class="col-md-3 text-center">
-						<h2><span class="underline-writing">WRITING</span></h2>
+						<h2><a href="#writing"><span class="underline-writing">WRITING</span></a></h2>
 					</div>
 					<div class="col-md-3 text-center">
-						<h2><span class="underline-contact">CONTACT</span></h2>
+						<h2><a href="#contact"><span class="underline-contact">CONTACT</span></a></h2>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="development">
+		<div class="development" id="development">
 			<div class="container">
-					<div class="development-title">
-						<h1>DEVELOP&#45;<br>MENT</h1>
-					</div>
-			<div class="development-description">
-				<p>Mobile web application, ABQ Street Art, an interactive map that allows users to search and locate murals, sculptures, and other public art in Albuquerque.</p>
-				<p>ABQ Street Art’s database was developed with MySQL, the front end UX and UI were created using a combination of HTML5, CSS3, Bootstrap, JavaScript, and jQuery. The backend was built using PHP7. Functionality was added to the project with the AngularJS framework. Geolocation capabilities were integrated using the Google Maps API.</p>
-			</div>
+				<div class="development-title">
+					<h1>DEVELOP&#45;<br>MENT</h1>
+				</div>
+				<div class="development-description">
+					<p>Mobile web application, ABQ Street Art, an interactive map that allows users to search and locate
+						murals, sculptures, and other public art in Albuquerque.</p>
+					<p>ABQ Street Art’s database was developed with MySQL, the front end UX and UI were created using a
+						combination of HTML5, CSS3, Bootstrap, JavaScript, and jQuery. The backend was built using PHP7.
+						Functionality was added to the project with the AngularJS framework. Geolocation capabilities were
+						integrated using the Google Maps API.</p>
+				</div>
 				<div class="row coding-skills">
-					<div class="col text-center">
-					<p>HTML5</p>
+					<div class="col-md-2 text-center">
+						<p><strong>HTML5</strong></p>
 					</div>
-					<div class="col text-center">
-					<p>CSS3</p>
+					<div class="col-md-2 text-center">
+						<p><strong>CSS3</strong></p>
 					</div>
-					<div class="col text-center">
-					<p>Javascript</p>
+					<div class="col-md-2 text-center">
+						<p><strong>Javascript</strong></p>
 					</div>
-					<div class="col text-center">
-					<p>PHP</p>
+					<div class="col-md-2 text-center">
+						<p><strong>PHP</strong></p>
 					</div>
-					<div class="col text-center">
-					<p>mySQL</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="design">
-			<div class="container">
-				<div class="row">
-					<div class="col design-title">
-						<h1>DESIGN</h1>
+					<div class="col-md-2 text-center">
+						<p><strong>mySQL</strong></p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="writing">
+		<div class="design" id="design">
 			<div class="container">
-				<div class="row">
-					<p><a href="https://theminimalistcreative.net/blog/the-relentless-pursuit-of-why-how-to-figure-out-what-you-want-and-forge-your-own-path"><u>The Relentless Pursuit of Why: How To Figure Out What You Want and Forge Your Own Path</u></a></p>
-					<p><a href="https://theminimalistcreative.net/blog/five-minute-journaling-exercise-to-diminish-anxiety-and-take-action"><u>What To Do When You Don't Know What To Do - Five Minute Journaling Exercises To Diminish Anxiety and Take Action</u></a></p>
-					<p><a href="https://theminimalistcreative.net/blog/the-systems-i-use-to-take-control-of-my-routine-and-make-time-for-projects-that-matter"><u>The Systems I Use To Take Control of My Routine and Make Time for Projects That Matter</u></a></p>
-				</div>
-					<div class="writing-title row">
-						<h1>WRITING</h1>
-					</div>
+				<div class="design-title">
+					<h1>DESIGN</h1>
 				</div>
 			</div>
-		<div class="contact">
+		</div>
+		<div class="writing" id="writing">
 			<div class="container">
 				<div class="row">
-					<a class="col contact-title">
+					<p><strong>I write articles about creativity and productivity on my blog, <a
+								href="https://theminimalistcreative.net/"><u>TheMinimalistCreative.net</u></a>. Here are some of
+							my favorites:</strong></p>
+					<br>
+					<p class="article-link"><a
+							href="https://theminimalistcreative.net/blog/the-relentless-pursuit-of-why-how-to-figure-out-what-you-want-and-forge-your-own-path"><u>The
+								Relentless Pursuit of Why: How To Figure Out What You Want and Forge Your Own Path</u></a></p>
+					<p class="article-link"><a
+							href="https://theminimalistcreative.net/blog/five-minute-journaling-exercise-to-diminish-anxiety-and-take-action"><u>What
+								To Do When You Don't Know What To Do - Five Minute Journaling Exercises To Diminish Anxiety and
+								Take Action</u></a></p>
+					<p class="article-link"><a
+							href="https://theminimalistcreative.net/blog/the-systems-i-use-to-take-control-of-my-routine-and-make-time-for-projects-that-matter"><u>The
+								Systems I Use To Take Control of My Routine and Make Time for Projects That Matter</u></a></p>
+				</div>
+				<div class="row writing-title">
+					<h1>WRITING</h1>
+				</div>
+			</div>
+		</div>
+		<div class="contact" id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-6">
 						<h1>CONTACT</h1>
 						<ol>
 							<li>Have an opinion.</li>
@@ -106,22 +119,39 @@
 						</ol>
 						<p>James Victore, Designer</p>
 						<a href="https://www.linkedin.com/in/samantharaeandrews/" target="_blank">
-						<img src="images/005-linkedin-logo.svg" alt="linkedin-icon" class="icon">
+							<img src="images/005-linkedin-logo.svg" alt="linkedin-icon" class="icon">
 						</a>
 						<a href="https://www.behance.net/samanthaandrews" target="_blank">
-						<img src="images/004-behance-logo.svg" alt="behance icon" class="icon">
-					</a>
+							<img src="images/004-behance-logo.svg" alt="behance icon" class="icon">
+						</a>
 						<a href="https://twitter.com/samandrews__" target="_blank">
-						<img src="images/002-twitter-logo-on-black-background.svg" alt="twitter-logo" class="icon">
+							<img src="images/002-twitter-logo-on-black-background.svg" alt="twitter-logo" class="icon">
 						</a>
 						<a href="https://www.instagram.com/samanthaandrews__/" target="_blank">
-						<img src="images/001-instagram-symbol.svg" alt="instagram-logo" class="icon">
+							<img src="images/001-instagram-symbol.svg" alt="instagram-logo" class="icon">
 						</a>
 						<a href="mailto:samantharaeandrews@gmail.com" target="_blank">
-						<img src="images/003-black-back-closed-envelope-shape.svg" alt="email-logo" class="icon">
-					</a>
-					<div class="col">
-						<p>contact form</p>
+							<img src="images/003-black-back-closed-envelope-shape.svg" alt="email-logo" class="icon">
+						</a>
+					</div>
+					<div class="col-6">
+						<div class="container contact-form">
+							<form action="action_page.php">
+
+								<label for="fname">First Name</label>
+								<input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+								<label for="lname">Last Name</label>
+								<input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+								<label for="subject">Subject</label>
+								<textarea id="subject" name="subject" placeholder="Write something.."
+											 style="height:200px"></textarea>
+
+								<input type="submit" value="Submit">
+
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
