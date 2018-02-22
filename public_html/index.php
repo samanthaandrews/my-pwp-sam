20acd66ef3042dff9cc794740d965cc65a4d7af9
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-22 10:14:42 -0700

    added reCAPTCHA keys

--- public_html/index.php
+++ public_html/index.php
@@ -170,7 +170,8 @@
 								<input type="submit" value="Submit">
 
 								<!-- reCAPTCHA -->
-								<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+								<div class="g-recaptcha" data-sitekey="6LfA-kcUAAAAAAOZfiRjlrOyNAOHt297n9yW5tcC
+"></div>
 
 								<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
 								<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
