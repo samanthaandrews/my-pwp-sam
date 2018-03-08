43e47fea33c3e79b309b702bf2297e4d132dbcdc
Samantha Andrews <samantharaeandrews@gmail.com>
2018-03-07 23:11:21 -0700

    cleaning up code and making sure everything is responsive

--- public_html/index.php
+++ public_html/index.php
@@ -50,195 +50,191 @@
 
 	<body>
 
-			<!-- begin welcome section -->
-			<div class="welcome">
-				<div class="container-fluid">
-					<div class="row">
-						<h1 class="pb-5">SAMANTHA <br> ANDREWS</h1>
+		<!-- begin welcome section -->
+		<div class="welcome">
+			<div class="container-fluid">
+				<div class="row">
+					<h1 class="pb-5">SAMANTHA <br> ANDREWS</h1>
+				</div>
+				<div class="row">
+					<div class="col-md-12 col-lg text-lg-center">
+						<h2><a href="#development"><span class="underline-development">DEVELOPMENT</span></a></h2>
 					</div>
-					<div class="row">
-						<div class="col-md-12 col-lg text-lg-center">
-							<h2><a href="#development"><span class="underline-development">DEVELOPMENT</span></a></h2>
-						</div>
-						<div class="col-md-12 col-lg text-lg-center">
-							<h2><a href="#design"><span class="underline-design">DESIGN</span></a></h2>
-						</div>
-						<div class="col-md-12 col-lg text-lg-center">
-							<h2><a href="#writing"><span class="underline-writing">WRITING</span></a></h2>
-						</div>
-						<div class="col-md-12 col-lg text-lg-center">
-							<h2><a href="#contact"><span class="underline-contact">CONTACT</span></a></h2>
+					<div class="col-md-12 col-lg text-lg-center">
+						<h2><a href="#design"><span class="underline-design">DESIGN</span></a></h2>
+					</div>
+					<div class="col-md-12 col-lg text-lg-center">
+						<h2><a href="#writing"><span class="underline-writing">WRITING</span></a></h2>
+					</div>
+					<div class="col-md-12 col-lg text-lg-center">
+						<h2><a href="#contact"><span class="underline-contact">CONTACT</span></a></h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- begin development section -->
+		<div class="development" id="development">
+			<div class="container-fluid">
+				<div class="row development-title">
+					<h1 class="pb-3">DEVELOP&#45;<br>MENT</h1>
+				</div>
+				<div class="pb-3">
+					<p>Recent graduate of the Fullstack Web Development Bootcamp at CNM. Worked amongst a team to build a
+						mobile web application, ABQ Street Art, an interactive map that allows users to
+						search and locate
+						murals, sculptures, and other public art in Albuquerque.</p>
+					<p>ABQ Street Art’s database was developed with MySQL, the front end UX and UI were created using a
+						combination of HTML5, CSS3, Bootstrap, JavaScript, and jQuery. The backend was built using PHP7.
+						Functionality was added to the project with the AngularJS framework. Geolocation capabilities were
+						integrated using the Google Maps API.</p>
+				</div>
+				<div class="row">
+					<div class="col-sm col-12 text-lg-center">
+						<p><strong>HTML5</strong></p>
+					</div>
+					<div class="col-sm col-12 text-lg-center">
+						<p><strong>CSS3</strong></p>
+					</div>
+					<div class="col-sm col-12 text-lg-center">
+						<p><strong>Javascript</strong></p>
+					</div>
+					<div class="col-sm col-12 text-lg-center">
+						<p><strong>PHP</strong></p>
+					</div>
+					<div class="col-sm col-12 text-lg-center">
+						<p><strong>mySQL</strong></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- begin design section -->
+		<div class="design" id="design">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-12 col-lg-4">
+						<h1 class="pb-3">DESIGN</h1>
+					</div>
+					<div class="col-sm-12 col-lg-8">
+						<div class="row d-flex justify-content-end">
+							<a data-fancybox="gallery" href="images/pdf/minimalist-creative-blog-design.pdf"><img
+									src="images/minimalist-creative.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/tessa-nickerson-branding.pdf"><img
+									src="images/tessa-nickerson.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/scintillating-fire-circus-branding.pdf"><img
+									src="images/scintillating-fire-circus.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/krista-lockwood-branding.pdf"><img
+									src="images/krista-lockwood.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/i-will-not-tone-it-down-zine.pdf"><img
+									src="images/i-will-not-tone-it-down-zine.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/emergency-contraception.pdf"><img
+									src="images/emergency-contraception.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/weekly-scraps-project.pdf"><img
+									src="images/weekly-scraps.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/digital-wallpapers.pdf"><img
+									src="images/digital-wallpapers.jpg" class="portfolio-image"></a>
+							<a data-fancybox="gallery" href="images/pdf/post-it-notes.pdf"><img
+									src="images/post-it-notes.jpg" class="portfolio-image"></a>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<!-- begin development section -->
-			<div class="development" id="development">
-				<div class="container-fluid">
-					<div class="row development-title">
-						<h1 class="pb-3">DEVELOP&#45;<br>MENT</h1>
+		<!-- begin writing section -->
+		<div class="writing" id="writing">
+			<div class="container-fluid">
+				<div class="row writing-title">
+					<h1 class="pb-3">WRITING</h1>
+				</div>
+				<div class="row">
+					<p><strong>I write articles about creativity and productivity on my blog, <a
+								href="https://theminimalistcreative.net/"><u>TheMinimalistCreative.net</u></a>. Here are
+							some
+							of
+							my favorites:</strong></p>
+					<br>
+					<p class="pl-2 pb-2"><a
+							href="https://theminimalistcreative.net/blog/the-relentless-pursuit-of-why-how-to-figure-out-what-you-want-and-forge-your-own-path"><u>The
+								Relentless Pursuit of Why: How To Figure Out What You Want and Forge Your Own Path</u></a>
+					</p>
+					<p class="pl-2 pb-2"><a
+							href="https://theminimalistcreative.net/blog/five-minute-journaling-exercise-to-diminish-anxiety-and-take-action"><u>What
+								To Do When You Don't Know What To Do - Five Minute Journaling Exercises To Diminish
+								Anxiety
+								and
+								Take Action</u></a></p>
+					<p class="pl-2 pb-2"><a
+							href="https://theminimalistcreative.net/blog/the-systems-i-use-to-take-control-of-my-routine-and-make-time-for-projects-that-matter"><u>The
+								Systems I Use To Take Control of My Routine and Make Time for Projects That Matter</u></a>
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<!-- begin contact section -->
+		<div class="contact" id="contact">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-12 col-md-12 col-lg-6">
+						<h1 class="pb-3">CONTACT</h1>
+						<blockquote class="text-right">
+							<i class="fa fa-quote-left"></i> Have an opinion.<br>
+							Care about something other than yourself.<br>
+							Make work that matters. <i class="fa fa-quote-right"></i>
+						</blockquote>
+						<p class="text-right pb-4">— James Victore, Designer</p>
+						<a href="mailto:samantharaeandrews@gmail.com" target="_blank">
+							<i class="fas fa-envelope-square fa-2x"></i>
+						</a>
+						<a href="https://www.instagram.com/samanthaandrews__/" target="_blank">
+							<i class="fab fa-instagram fa-2x"></i>
+						</a>
+						<a href="https://twitter.com/samandrews__" target="_blank">
+							<i class="fab fa-twitter-square fa-2x"></i>
+						</a>
+						<a href="https://www.behance.net/samanthaandrews" target="_blank">
+							<i class="fab fa-behance-square fa-2x"></i>
+						</a>
+						<a href="https://github.com/samanthaandrews/" target="_blank">
+							<i class="fab fa-github-square fa-2x"></i>
+						</a>
+						<a href="https://medium.com/@samanthaandrews" target="_blank">
+							<i class="fab fa-medium-m fa-2x"></i>
+						</a>
+						<a href="https://www.linkedin.com/in/samantharaeandrews/" target="_blank">
+							<i class="fab fa-linkedin fa-2x"></i>
+						</a>
 					</div>
-					<div class="development-description">
-						<p>Recent graduate of the Fullstack Web Development Bootcamp at CNM. Worked amongst a team to build a
-							mobile web application, ABQ Street Art, an interactive map that allows users to
-							search and locate
-							murals, sculptures, and other public art in Albuquerque.</p>
-						<p>ABQ Street Art’s database was developed with MySQL, the front end UX and UI were created using a
-							combination of HTML5, CSS3, Bootstrap, JavaScript, and jQuery. The backend was built using PHP7.
-							Functionality was added to the project with the AngularJS framework. Geolocation capabilities were
-							integrated using the Google Maps API.</p>
-					</div>
-					<div class="row coding-skills">
-						<div class="col-sm col-12 text-lg-center">
-							<p><strong>HTML5</strong></p>
-						</div>
-						<div class="col-sm col-12 text-lg-center">
-							<p><strong>CSS3</strong></p>
-						</div>
-						<div class="col-sm col-12 text-lg-center">
-							<p><strong>Javascript</strong></p>
-						</div>
-						<div class="col-sm col-12 text-lg-center">
-							<p><strong>PHP</strong></p>
-						</div>
-						<div class="col-sm col-12 text-lg-center">
-							<p><strong>mySQL</strong></p>
-						</div>
+					<div class="col-sm-12 col-md-12 col-lg-6 form">
+						<form class="form pt-2" role="form" id="my-pwp-form" action="php/mailer.php" method="post">
+
+							<label for="name">Name</label>
+							<input type="text" id="name" name="name" placeholder="Your name..">
+
+							<label for="email">Email Address</label>
+							<input type="email" id="email" name="email" placeholder="Your email address..">
+
+							<label for="subject">Subject</label>
+							<input type="text" id="subject" name="subject" placeholder="Subject..">
+
+							<label for="message">Message</label>
+							<textarea id="message" name="message" placeholder="Write something.."
+										 style="height:15vh"></textarea>
+
+							<!-- reCAPTCHA -->
+							<div class="g-recaptcha" data-sitekey="6LfA-kcUAAAAAAOZfiRjlrOyNAOHt297n9yW5tcC"></div>
+
+							<input type="submit" value="Submit" name="enter">
+
+							<input type="reset" value="Reset">
+						</form>
+						<!--empty area for form error/success output-->
+						<div id="output-area"></div>
 					</div>
 				</div>
 			</div>
-
-			<!-- begin design section -->
-			<div class="design" id="design">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-lg-4 d-flex align-items-end">
-							<h1>DESIGN</h1>
-						</div>
-						<div class="col-lg-8 align-items-center">
-							<div class="row d-flex justify-content-end">
-								<a data-fancybox="gallery" href="images/pdf/minimalist-creative-blog-design.pdf"><img
-										src="images/minimalist-creative.jpg" class="portfolio-image"></a>
-								<a data-fancybox="gallery" href="images/pdf/tessa-nickerson-branding.pdf"><img
-										src="images/tessa-nickerson.jpg" class="portfolio-image"></a>
-								<a data-fancybox="gallery" href="images/pdf/scintillating-fire-circus-branding.pdf"><img
-										src="images/scintillating-fire-circus.jpg" class="portfolio-image"></a>
-							</div>
-							<div class="row d-flex justify-content-end">
-								<a data-fancybox="gallery" href="images/pdf/krista-lockwood-branding.pdf"><img
-										src="images/krista-lockwood.jpg" class="portfolio-image"></a>
-								<a data-fancybox="gallery" href="images/pdf/i-will-not-tone-it-down-zine.pdf"><img
-										src="images/i-will-not-tone-it-down-zine.jpg" class="portfolio-image"></a>
-								<a data-fancybox="gallery" href="images/pdf/emergency-contraception.pdf"><img
-										src="images/emergency-contraception.jpg" class="portfolio-image"></a>
-							</div>
-							<div class="row d-flex justify-content-end">
-								<a data-fancybox="gallery" href="images/pdf/weekly-scraps-project.pdf"><img
-										src="images/weekly-scraps.jpg" class="portfolio-image"></a>
-								<a data-fancybox="gallery" href="images/pdf/digital-wallpapers.pdf"><img
-										src="images/digital-wallpapers.jpg" class="portfolio-image"></a>
-								<a data-fancybox="gallery" href="images/pdf/post-it-notes.pdf"><img
-										src="images/post-it-notes.jpg" class="portfolio-image"></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<!-- begin writing section -->
-			<div class="writing" id="writing">
-				<div class="container-fluid">
-					<div class="row writing-title">
-						<h1 class="pb-3">WRITING</h1>
-					</div>
-					<div class="row">
-						<p><strong>I write articles about creativity and productivity on my blog, <a
-									href="https://theminimalistcreative.net/"><u>TheMinimalistCreative.net</u></a>. Here are
-								some
-								of
-								my favorites:</strong></p>
-						<br>
-						<p class="article-link"><a
-								href="https://theminimalistcreative.net/blog/the-relentless-pursuit-of-why-how-to-figure-out-what-you-want-and-forge-your-own-path"><u>The
-									Relentless Pursuit of Why: How To Figure Out What You Want and Forge Your Own Path</u></a>
-						</p>
-						<p class="article-link"><a
-								href="https://theminimalistcreative.net/blog/five-minute-journaling-exercise-to-diminish-anxiety-and-take-action"><u>What
-									To Do When You Don't Know What To Do - Five Minute Journaling Exercises To Diminish
-									Anxiety
-									and
-									Take Action</u></a></p>
-						<p class="article-link"><a
-								href="https://theminimalistcreative.net/blog/the-systems-i-use-to-take-control-of-my-routine-and-make-time-for-projects-that-matter"><u>The
-									Systems I Use To Take Control of My Routine and Make Time for Projects That Matter</u></a>
-						</p>
-					</div>
-				</div>
-			</div>
-
-			<!-- begin contact section -->
-			<div class="contact" id="contact">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-sm-12 col-md-12 col-lg-6">
-							<h1 class="pb-3">CONTACT</h1>
-							<blockquote class="text-right">
-								<i class="fa fa-quote-left"></i> Have an opinion.<br>
-								Care about something other than yourself.<br>
-								Make work that matters. <i class="fa fa-quote-right"></i>
-							</blockquote>
-							<p class="text-right pb-4">— James Victore, Designer</p>
-							<a href="mailto:samantharaeandrews@gmail.com" target="_blank">
-								<i class="fas fa-envelope-square fa-2x"></i>
-							</a>
-							<a href="https://www.instagram.com/samanthaandrews__/" target="_blank">
-								<i class="fab fa-instagram fa-2x"></i>
-							</a>
-							<a href="https://twitter.com/samandrews__" target="_blank">
-								<i class="fab fa-twitter-square fa-2x"></i>
-							</a>
-							<a href="https://www.behance.net/samanthaandrews" target="_blank">
-								<i class="fab fa-behance-square fa-2x"></i>
-							</a>
-							<a href="https://github.com/samanthaandrews/" target="_blank">
-								<i class="fab fa-github-square fa-2x"></i>
-							</a>
-							<a href="https://medium.com/@samanthaandrews" target="_blank">
-								<i class="fab fa-medium-m fa-2x"></i>
-							</a>
-							<a href="https://www.linkedin.com/in/samantharaeandrews/" target="_blank">
-								<i class="fab fa-linkedin fa-2x"></i>
-							</a>
-						</div>
-						<div class="col-sm-12 col-md-12 col-lg-6 form">
-							<form class="form" role="form" id="my-pwp-form" action="php/mailer.php" method="post">
-
-								<label for="name">Name</label>
-								<input type="text" id="name" name="name" placeholder="Your name..">
-
-								<label for="email">Email Address</label>
-								<input type="email" id="email" name="email" placeholder="Your email address..">
-
-								<label for="subject">Subject</label>
-								<input type="text" id="subject" name="subject" placeholder="Subject..">
-
-								<label for="message">Message</label>
-								<textarea id="message" name="message" placeholder="Write something.."
-											 style="height:15vh"></textarea>
-
-								<!-- reCAPTCHA -->
-								<div class="g-recaptcha" data-sitekey="6LfA-kcUAAAAAAOZfiRjlrOyNAOHt297n9yW5tcC"></div>
-
-								<input type="submit" value="Submit" name="enter">
-
-								<input type="reset" value="Reset">
-							</form>
-							<!--empty area for form error/success output-->
-							<div id="output-area"></div>
-						</div>
-					</div>
-				</div>
-			</div>
+		</div>
 	</body>
 </html>
